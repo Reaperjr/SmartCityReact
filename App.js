b92a1e4c172e14a417dd087ae68dbe87119a9fdf
Nuno <nunopaotrigo@hotmail.com>
2020-06-23 16:59:14 +0100

    init proj

---
+++ App.js
@@ -0,0 +1,19 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Login from './src/screens/login/login';
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ src/translation/translations.js
@@ -0,0 +1,11 @@
+export const translations = {
+  en: {
+    WELCOME: 'Welcome to Localization',
+    BUTTON: 'b en'
+  },
+  pt: {
+    WELCOME: 'Bem-vindo à multi-lingua',
+    BUTTON: 'b pt'
+  }
+};
+
