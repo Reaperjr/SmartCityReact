aa0e602f13227a33bec8e9eceb7b9703573acc0f
Nuno <nunopaotrigo@hotmail.com>
2020-07-16 14:21:29 +0100

    updatepoint not done

--- src/screens/Note/Listagem.js
+++ src/screens/Note/Listagem.js
@@ -9,6 +9,7 @@ import React, { Component,useContext } from 'react';
 import { StyleSheet, Platform, View, Button, TouchableWithoutFeedback, Image, Text, TextInput, TouchableOpacity, Alert, YellowBox, FlatList } from 'react-native';
 import * as NoteDB from '../../db/NoteDB';
 import {LocalizationContext} from '../../translation/LocalizationContext';
+import translations from '../../translation/translations';
 YellowBox.ignoreWarnings([
   'Non-serializable values were found in the navigation state',
 ]);
@@ -55,10 +56,10 @@ reloadData = () => {
                renderItem={({ item, index }) => (
                   <TouchableOpacity onPress={ () => this.actionOnRow(item)}>
                   <View style={{ backgroundColor: index % 2 == 0 ? 'powderblue' : 'skyblue'}}>
-                    <Text style={{ fontSize: 18, margin: 5 }}>{item.title}</Text>
-                    <Text style={{ fontSize: 18, margin: 5 }}>{item.local}</Text>
-                    <Text style={{ fontSize: 18, margin: 5 }} numberOfLines={2}>{item.date.toLocaleString()}</Text>
-                    <Text style={{ fontSize: 18, margin: 5 }}>{item.description}</Text>
+               <Text style={{ fontSize: 18, margin: 5 }}>{translations.title}: {item.title}</Text>
+                    <Text style={{ fontSize: 18, margin: 5 }}>{translations.local}: {item.local}</Text>
+                    <Text style={{ fontSize: 18, margin: 5 }} numberOfLines={2}>{translations.date}: {item.date.toLocaleString()}</Text>
+                    <Text style={{ fontSize: 18, margin: 5 }}>{translations.desc}: {item.description}</Text>
                     </View>
                   </TouchableOpacity>
                   )}/>
